1f48346b33340966fa7336cc15b02c3107d1305b
anku-26 <2654anku@gmail.com>
2025-04-19 04:09:22 +0530

    landing_ page

--- project/src/components/Logo.tsx
+++ project/src/components/Logo.tsx
@@ -12,7 +12,7 @@ export const Logo: React.FC<LogoProps> = ({ size = 40, className = '' }) => {
       style={{ width: size, height: size }}
     >
       <img 
-        src="/src/assets/nearbux-logo.jpg" 
+        src="/nearbux-logo.jpg" 
         alt="NearBux Logo" 
         className="w-full h-full object-cover"
       />
